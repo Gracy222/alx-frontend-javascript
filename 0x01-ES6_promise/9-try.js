f13d6a6aaacc8fcf600e603976464c5e5926aea6
Gracy222 <gbamidele23@gmail.com>
2024-10-03 19:27:13 +0000

    ES6_promise

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,11 +2,13 @@ export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-    queue.push(mathFunction());
-  } catch (err) {
-    queue.push(err.toString());
+    const output = mathFunction();
+    queue.push(output);
+  } catch (error) {
+    queue.push(error.toString());
   } finally {
     queue.push('Guardrail was processed');
   }
+
   return queue;
 }
